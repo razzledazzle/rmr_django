10339307557c6314a458fe6beb96293942f610b7
sk <snaffi777@gmail.com>
2015-12-03 13:53:38 +0300

    remove login_required decorator

--- rmr/views/__init__.py
+++ rmr/views/__init__.py
@@ -7,19 +7,8 @@ def anonymous_required(fn):
     def _fn(self, request, *args, **kwargs):
         if request.user.is_authenticated():
             raise ClientError(
-                'User already registered',
-                code='user_already_registered',
-            )
-        return fn(self, request, *args, **kwargs)
-    return _fn
-
-
-def login_required(fn):
-    def _fn(self, request, *args, **kwargs):
-        if request.user.is_anonymous():
-            raise ClientError(
-                'User not authenticated',
-                code='user_not_authenticated',
+                'You can not perform this action while logged in.',
+                code='you_can_not_perform_this_action_while_logged_in',
             )
         return fn(self, request, *args, **kwargs)
     return _fn
